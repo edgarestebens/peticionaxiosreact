4bbab914f60505f476703c39f45b2499978b0130
edgarestebens <colachoedgar@hotmail.com>
2019-01-12 13:21:32 -0500

    instalacion de boostrap en react y ejemplo de boton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios' 
+import {Button} from 'react-bootstrap'
 import './App.css';
 
 class App extends Component {
@@ -24,6 +25,7 @@ class App extends Component {
   render() {
     return (
       <div>
+       <Button bsStyle="primary">Primary</Button>
         <ul>
           {this.state.names.map(name =>
             <li key={name}>{name}</li>
