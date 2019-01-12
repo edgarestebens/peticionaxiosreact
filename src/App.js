1c9214f617618114b33710f151c8610ba82bf932
edgarestebens <colachoedgar@hotmail.com>
2019-01-12 13:03:50 -0500

    importar axios y llamar el json de nombres

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios' 
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state={
+      names:[]
+    };
+
+    axios.get("http://localhost:3001/names")
+      .then(response =>{
+        this.setState({
+          names:response.data
+        })
+      })
+      .catch(error =>{
+        console.log(error);
+      })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ul>
+          {this.state.names.map(name =>
+            <li key={name}>{name}</li>
+            )}
+        </ul>
       </div>
     );
   }
